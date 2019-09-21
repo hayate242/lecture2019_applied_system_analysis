499313324f1b6759c12de942746b0d80576ca2cc
中山颯 <sippu242@gmail.com>
2019-09-21 14:34:55 +0900

    sytemA dosen't working

--- kadaiB/systemA/MM3.cpp
+++ kadaiB/systemA/MM3.cpp
@@ -7,7 +7,7 @@ std::tuple<double, double> MM3::simulation(double lambda, double mu, double star
 {
     // 初期化
     vector<double> service;    // 窓口
-    queue<double> systems[SERVICE_NUM];
+    queue<double> systems[SERVICE_NUM]; //待ち行列
     int selectedLine;
     Random random;             // ランダムクラスの宣言
     SimStat simStat = StandBy; // 過渡状態、過渡状態以降を定義
@@ -18,18 +18,13 @@ std::tuple<double, double> MM3::simulation(double lambda, double mu, double star
     stayTime = 0.0;
     visitors = 0.0;
     // 処理開始（最初のイベントをイベントキューに登録）
+    event.selectedLine = random.genRand(0, 2);
     event.time = currentTime + random.expDistribution(lambda);
     event.eventState = ARRIVE;
     eventQueue.push(event);
     // 窓口初期化
     for (int i = 0; i < SERVICE_NUM; i++){
         service.push_back(-1.0);
-    }
-    systems[0].push(3.0);
-    for (int i = 0; i < SERVICE_NUM; i++)
-    {
-        systems[i].push(12.0);
-        systems[i].push(2.0);
     }
 
     // 終了時間まで繰り返す
@@ -45,11 +40,10 @@ std::tuple<double, double> MM3::simulation(double lambda, double mu, double star
         // 到着イベントの場合
         case ARRIVE:
             // 待ち行列への登録
-            selectedLine = random.genRand(0, 2);
-            systems[selectedLine].push(event.time);
+            systems[event.selectedLine].push(event.time);
 
             // イベントキューへのサービス到着イベントの登録
-            event.selectedLine = selectedLine;
+            event.selectedLine = random.genRand(0, 2);
             event.time = currentTime + random.expDistribution(lambda);
             event.eventState = ARRIVE;
             eventQueue.push(event);
@@ -65,10 +59,19 @@ std::tuple<double, double> MM3::simulation(double lambda, double mu, double star
                 simEndJobs = 0;
             }
             // 統計処理（待ち時間，客数等のカウント）
-            visitors += systems[0].size() + systems[1].size() + systems[2].size();
-            printf("visitors = %f\n", visitors);
+            int cnt_in_service = 0;
+            for (int i = 0; i < SERVICE_NUM; i++)
+            {
+                if(service[i] != -1.0 ){
+                    cnt_in_service++;
+                }
+            }
+            visitors += cnt_in_service + systems[0].size() + systems[1].size() + systems[2].size();
+            // printf("visitors = %f\n", visitors);
+
+            printf("%f - %f ,staytime = %f\n", currentTime, service[event.selectedLine], currentTime - service[event.selectedLine]);
             stayTime += currentTime - service[event.selectedLine];
-            printf("stayTime = %f\n", stayTime);
+            // printf("stayTime = %f\n", stayTime);
             simEndJobs++;
 
             // 窓口のクリア（先頭の要素だけ削除）
@@ -81,26 +84,19 @@ std::tuple<double, double> MM3::simulation(double lambda, double mu, double star
         // 3つの列の窓口で，並んでる列の窓口に人がいなくて，待ち行列に人がいる場合
         if (service[event.selectedLine] == -1.0 && systems[event.selectedLine].empty() == false)
         {
-            // printf("%f\n", service[event.selectedLine]);
             // 待ち行列の先頭から窓口への移動
-            service.push_back(systems[selectedLine].front());
-            systems[selectedLine].pop();
+            service[event.selectedLine] = systems[event.selectedLine].front();
+            systems[event.selectedLine].pop();
             // debug
             // {for(int i=0;i<service.size();i++) printf("%f,",service[i]);}
             // printf("size = %lu\n", service.size());
             // イベントキューへのサービス到着イベントの登録
-            selectedLine = random.genRand(0, 2);
-            event.selectedLine = selectedLine;
+            event.selectedLine = random.genRand(0, 2);
             event.time = currentTime + random.expDistribution(mu);
             event.eventState = FINISH;
             eventQueue.push(event);
         }
-        // for (int i = 0; i < 3; i++){
-        //     for (int k = 0; k < systems[i].size(); k++){
-        //         printf("%f, ", systems[i]);
-        //     }
-        // }
-        // printf("\n");
+
     }
     // 結果の返却
     // return std::forward_as_tuple(システム内客数, システム内時間);
